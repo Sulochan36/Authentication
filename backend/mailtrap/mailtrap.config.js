1f8b845679ba61596646e25a8dd3732c0cc386ca
Sulochan Mahajan <sulochanmahajan@gmail.com>
2025-12-15 16:52:05 +0530

    first commit

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import { MailtrapClient } from 'mailtrap';
+
+dotenv.config();
+
+
+
+
+
+export const mailtrapClient = new MailtrapClient({
+    token: process.env.MAIL_TOKEN
+});
+
+export const sender = {
+    email: "hello@demomailtrap.com",
+    name: "Sulochan",
+};
+
+
